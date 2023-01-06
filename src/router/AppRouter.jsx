52b58fae0fe54eff7dfa9b9449011fef3f701256
Dajan Medina <dmedina2150dev@gmail.com>
2023-01-05 21:59:57 -0300

    crear, aplicar y usar ruta publica

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,12 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import { HeroesRoutes } from '../heroes';
 import { Login } from '../auth';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<Login />} />
+                <Route path="login" element={
+                    <PublicRoute>
+                        <Login />
+                    </PublicRoute>
+                }>
+
+                </Route>
 
                 {/* TODO de esta forma creamos o se ordenan las rutas privadas */}
                 <Route path="/*" element={
@@ -18,6 +25,7 @@ export const AppRouter = () => {
                     </PrivateRoute>
                 }>
 
+                {/* <Route path="login" element={<Login />} /> */}
                 {/* <Route path="/*" element={<HeroesRoutes />} /> */}
                 </Route>
             </Routes>
